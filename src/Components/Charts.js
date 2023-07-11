5bd78e42e6fbc8753eda870709e55f276bc6adaa
Tamizh-1153 <tamizh1153@gmail.com>
2023-07-11 12:39:14 +0530

    error correction

--- src/Components/Charts.js
+++ src/Components/Charts.js
@@ -8,7 +8,7 @@ const Charts = () => {
         Chart.js is a third party plugin that is used to generate the charts in
         this theme. The charts below have been customized - for further
         customization options, please visit the{" "}
-        <a target="_blank" href="https://www.chartjs.org/docs/latest/">
+        <a href="https://www.chartjs.org/docs/latest/">
           official Chart.js documentation
         </a>
         .
--- src/Components/Tables.js
+++ src/Components/Tables.js
@@ -7,7 +7,7 @@ const Tables = () => {
       <p className="mb-4">
         DataTables is a third party plugin that is used to generate the demo
         table below. For more information about DataTables, please visit the{" "}
-        <a target="_blank" href="https://datatables.net">
+        <a href="https://datatables.net">
           official DataTables documentation
         </a>
         .
